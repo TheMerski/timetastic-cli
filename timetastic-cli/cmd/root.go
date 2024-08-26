f35ee8f1fe27b54e56f27d99dab31c345d167789
Merwin Hill <merwin@q42.nl>
2024-08-26 21:05:25 +0200

    feat: Add version information

--- timetastic-cli/cmd/root.go
+++ timetastic-cli/cmd/root.go
@@ -3,14 +3,16 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"runtime/debug"
 
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "timetastic",
-	Short: "Manage timetastic bookings from the command line",
-	Long:  `Create your timetastic bookings from the command line`,
+	Use:     "timetastic",
+	Short:   "Manage timetastic bookings from the command line",
+	Long:    `Create your timetastic bookings from the command line`,
+	Version: "development",
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.Help()
 	},
@@ -20,5 +22,15 @@ func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
+	}
+}
+
+func init() {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		rootCmd.Version = "unknown"
+	}
+	if info.Main.Version != "" {
+		rootCmd.Version = info.Main.Version
 	}
 }
